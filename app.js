9daa999400478a68654832ff07c3ce22d8d20f1b
Rômulo da Cruz Vieira <romulo.vieira@gazin.com.br>
2020-03-27 11:34:09 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+//rotas da aplicação
+const indexUsuarioRouter = require('./routes/usuarios/index')
+
+const app = express();
+//pacotes a ser utilizados na aplicação
+const expressEjsLayouts = require('express-ejs-layouts')
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+//pacotes para que o express utilize essa biblioteca de layouts
+app.use(expressEjsLayouts);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+//rotas da aplicação
+app.use('/usuarios',indexUsuarioRouter)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
